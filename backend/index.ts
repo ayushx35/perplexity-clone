4d5d0874dc41df4d1ae77a61e3ecbc64e691918e
Ayushmaan Singh <ayushmaansingh0305@gmail.com>
2026-05-21 22:20:27 +0530

    update backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,22 @@
-const app = require('express');
+import express from "express";
+import { tavily } from "@tavily/core";
 
-app.post("/chat", (req, res) => {
-    res.send("Hello server");
+const client = tavily({ apiKey: process.env.TAVILLI_API_KEY });
+
+
+
+const app = express();
+app.use(express.json());
+
+app.post("/chat", async (req, res) => {
+    const query = req.body.query;
+    const reply = await client.search(query, {
+        searchDepth: "advanced"
+    });
+    const result = reply.results;
+
 });
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
-});
+}); 
